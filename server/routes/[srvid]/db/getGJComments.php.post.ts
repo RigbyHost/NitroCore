b3f9434ab49f98ae3cbca337375603c9eb550e7b
M41den <38406005+m41denx@users.noreply.github.com>
2025-09-05 23:42:55 +0300

    getComments

---
+++ server/routes/[srvid]/db/getGJComments.php.post.ts
@@ -0,0 +1,34 @@
+import {initMiddleware} from "~/gdps_middleware/init_gdps";
+import {z} from "zod";
+import {CommentController} from "~~/controller/CommentController";
+
+
+export default defineEventHandler({
+    onRequest: [initMiddleware],
+
+    handler: async (event) => {
+        const post = usePostObject<z.infer<typeof requestSchema>>(await readFormData(event))
+
+        const {data, success} = requestSchema.safeParse(post)
+
+        if (!success)
+            return await event.context.connector.error(-1, "Bad Request")
+
+        const mode = data.mode ? "likes": "postedTime"
+
+        const commentController = new CommentController(event.context.drizzle)
+        const comments = await commentController.getAllLevelComments(data.levelID, mode, data.page)
+
+        return await event.context.connector.comments.getLevelComments(
+            comments,
+            await commentController.countLevelComments(data.levelID),
+            data.page
+        )
+    }
+})
+
+const requestSchema = z.object({
+    levelID: z.number(),
+    page: z.number().gte(0).optional().default(0),
+    mode: z.number().optional()
+})
---
+++ server/routes/[srvid]/db/getGJComments21.php.post.ts
@@ -0,0 +1,2 @@
+import route from "~/routes/[srvid]/db/getGJComments.php.post"
+export default route
