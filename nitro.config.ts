a941ac166b4c99ced37d40ee9e4585f14ba2a48e
M41den <38406005+m41denx@users.noreply.github.com>
2025-10-13 22:18:38 +0300

    return original config for base

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,7 +1,4 @@
 // https://nitro.build/config
-
-
-
 export default defineNitroConfig({
     compatibilityDate: "2025-10-10",
     srcDir: "server",
@@ -15,15 +12,28 @@ export default defineNitroConfig({
     },
     storage: {
         savedata: {
-            driver: "vercel-blob",
-            access: "public", // DO NOT CHANGE, THIS IS MANDATORY AND IS NOT A BUG: https://unstorage.unjs.io/drivers/vercel#vercel-blob
-            // token: process.env.BLOB_READ_WRITE_TOKEN, // Optional
+            driver: "s3",
+            accessKeyId: process.env.S3_ACCESS_KEY,
+            secretAccessKey: process.env.S3_SECRET,
+            endpoint: process.env.S3_URL,
+            bucket: process.env.S3_BUCKET,
+            region: process.env.S3_REGION || "us-east-1",
         },
-        // This driver doesn't exist in upstream unstorage, so it is loaded dynamically as storage plugin asn always
-        // needs process.env.EDGE_CONFIG
-        // config: {
-        //     driver: "storage-vercel-edgeconfig",
-        //     // url: process.env.EDGE_CONFIG // Optional
-        // }
+        config: {
+            driver: "redis",
+            url: process.env.REDIS_URL,
+        }
+    },
+    devStorage: {
+        savedata: {
+            driver: "fs-lite",
+            base: "./_savedata"
+        },
+        config: { // DO NOT REMOVE: AUTOPOPULATED BY VITEST
+            driver: "redis",
+            host: process.env.STORAGE_HOST || 'valkey',
+            port: Number(process.env.STORAGE_PORT) || 6379,
+            password: process.env.STORAGE_PASSWORD || ''
+        }
     }
 });
