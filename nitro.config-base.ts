da734576b08abf3d4336909e3715065685471271
M41den <38406005+m41denx@users.noreply.github.com>
2025-10-13 23:20:37 +0300

    More docs

---
+++ nitro.config-base.ts
@@ -0,0 +1,39 @@
+// https://nitro.build/config
+export default defineNitroConfig({
+    compatibilityDate: "2025-10-10",
+    srcDir: "server",
+    routeRules: {
+        "/**": {cors: true}
+    },
+    experimental: {
+        asyncContext: true,
+        database: true,
+        tasks: true,
+    },
+    storage: {
+        savedata: {
+            driver: "s3",
+            accessKeyId: process.env.S3_ACCESS_KEY,
+            secretAccessKey: process.env.S3_SECRET,
+            endpoint: process.env.S3_URL,
+            bucket: process.env.S3_BUCKET,
+            region: process.env.S3_REGION || "us-east-1",
+        },
+        config: {
+            driver: "redis",
+            url: process.env.REDIS_URL,
+        }
+    },
+    devStorage: {
+        savedata: {
+            driver: "fs-lite",
+            base: "./_savedata"
+        },
+        config: { // DO NOT REMOVE: AUTOPOPULATED BY VITEST
+            driver: "redis",
+            host: process.env.STORAGE_HOST || 'valkey',
+            port: Number(process.env.STORAGE_PORT) || 6379,
+            password: process.env.STORAGE_PASSWORD || ''
+        }
+    }
+});
