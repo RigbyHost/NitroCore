43bbcd2bb5862731227c8fca1b0b3ea52b92575d
M41den <38406005+m41denx@users.noreply.github.com>
2025-10-13 21:26:24 +0300

    Support Vercel Deployment

---
+++ docs/deploy/vercel.md
@@ -0,0 +1,21 @@
+# Запуск NitroCore на ▲ [Vercel](https://vercel.com)
+
+NitroCore без проблем запускается на большинстве облачных провайдеров, включая Vercel.
+
+Данное руководство предназначено для тех, кто ни разу в жизни Vercel не видел. Если вы знаете что делаете, то TLDR: просто замените команду сборки на `bun run build:vercel` 
+
+1. Зарегистрируйте аккаунт на Vercel
+
+2. Нажмите **Add New... → Project**
+
+3. Выберите **Import Third-Party Git Repository →**
+   ![](_img/img.png)
+
+4. Укажите URL данного репозитория и нажмите **Continue**
+   ![](_img/img2.png)
+
+5. Дальше измените **Build Command** в Build and Output Settings на `bun run build:vercel`, нажмите **Deploy**.
+
+   Если Vercel жалуется на `Project names can be up to 100 characters long and must be lowercase...`, то просто переименуйте проект, чтобы в нем не было заглавных букв
+   ![](_img/img3.png)
+   
---
+++ server/plugins/storage-vercel-edgeconfig.ts
@@ -0,0 +1,33 @@
+import {defineDriver, normalizeKey, joinKeys} from "unstorage";
+import {EdgeConfigClient, createClient} from "@vercel/edge-config"
+
+export default defineNitroPlugin(() => {
+    useStorage().mount("config", storageDriver({}))
+})
+
+const storageDriver = defineDriver<{
+    base?: string,
+    url?: string
+}, EdgeConfigClient>((opts) => {
+    const base = normalizeKey(opts?.base)
+    const r = (...keys: string[]) => joinKeys(base, ...keys)
+
+    let _client: EdgeConfigClient
+    const getClient = () => {
+        if (!_client) {
+            const url = opts.url || process.env.EDGE_CONFIG
+            if (!url)
+                throw new Error(`[unstorage] [vercel-edgeconfig] No URL provided and EDGE_CONFIG environment variable not set`)
+            _client = createClient(url)
+        }
+        return _client
+    }
+
+    return {
+        name: "vercel-edgeconfig",
+        getInstance: getClient,
+        hasItem: (key) => getClient().has(r(key)),
+        getItem: (key) => getClient().get(r(key)),
+        getKeys: () => getClient().getAll()
+    }
+})
