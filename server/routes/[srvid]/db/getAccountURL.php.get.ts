53539bf8cef5e5f901747db4a3f9167104413f8c
M41den <38406005+m41denx@users.noreply.github.com>
2025-10-14 12:10:17 +0300

    Crunchable routes

--- server/routes/[srvid]/db/getAccountURL.php.get.ts
+++ server/routes/[srvid]/db/getAccountURL.php.get.ts
@@ -1,4 +1,4 @@
 export default defineEventHandler( event => {
     const srvid = getRouterParam(event, "srvid")!
-    return sendRedirect(event, `https://${getRequestHost(event)}/${srvid}/db`, 301)
+    return `https://${getRequestHost(event)}/${srvid}/db`
 })
