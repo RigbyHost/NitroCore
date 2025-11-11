697e26709dac1f63c0ded51099f55175175d012f
M41den <38406005+m41denx@users.noreply.github.com>
2025-11-11 20:51:33 +0300

    Replace env DEBUG -> VERBOSE to avoid nitro debug

--- server/utils/useLogger.ts
+++ server/utils/useLogger.ts
@@ -1,7 +1,7 @@
 import winston from "winston"
 
 const logger = winston.createLogger({
-    level: process.env.DEBUG ? "info": "error",
+    level: process.env.VERBOSE ? "info": "error",
     format: winston.format.simple(),
     transports: [
         new winston.transports.Console(),
